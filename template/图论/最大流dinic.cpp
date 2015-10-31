18c12d1f87686761c6ca2cd6eed096fdd606d668
xiang578 <xiang578@foxmail.com>
2015-10-31 16:13:54 +0800

    网络流相关

---
+++ template/图论/最大流dinic.cpp
@@ -0,0 +1,106 @@
+//poj 1273 ×î´óÁ÷
+#include<stdio.h>
+#include<string.h>
+#include<queue>
+#include<vector>
+using namespace std;
+const int N=1024;
+const int inf=1<<24;
+
+struct Edge
+{
+    int from,to,cap,flow;
+};
+vector<Edge>edges;
+vector<int>G[N];
+int n,m,s,t;
+int vis[N];
+int d[N];
+int cur[N];
+
+void AddEdge(int from,int to,int cap)
+{
+    Edge tp;
+    tp.from=from,tp.to=to,tp.cap=cap,tp.flow=0;
+    edges.push_back(tp);
+
+    tp.from=to,tp.to=from,tp.cap=0,tp.flow=0;
+    edges.push_back(tp);
+
+    int g_size=edges.size();
+    G[from].push_back(g_size-2);
+    G[to].push_back(g_size-1);
+}
+
+bool BFS()
+{
+    memset(vis,0,sizeof(vis));
+    queue<int>Q;
+    Q.push(s);
+    d[s]=0;
+    vis[s]=1;
+    while(!Q.empty())
+    {
+        int x=Q.front();
+        Q.pop();
+        for(int i=0; i<G[x].size(); i++)
+        {
+            Edge &e=edges[G[x][i]];
+            if(!vis[e.to]&&e.cap>e.flow)
+            {
+                vis[e.to]=1;
+                d[e.to]=d[x]+1;
+                Q.push(e.to);
+            }
+        }
+    }
+    return vis[t];
+}
+
+int DFS(int x,int a)
+{
+    if(x==t||a==0) return a;
+    int flow=0,f;
+    for(int &i=cur[x]; i<G[x].size(); i++)
+    {
+        Edge &e=edges[G[x][i]];
+        if(d[x]+1==d[e.to]&&(f=DFS(e.to,min(a,e.cap-e.flow)))>0)
+        {
+            e.flow+=f;
+            edges[G[x][i]^1].flow-=f;
+            flow+=f;
+            a-=f;
+            if(a==0) break;
+        }
+    }
+    return flow;
+}
+
+int Maxflow(int st,int ed)
+{
+    int flow=0;
+    while(BFS())
+    {
+        memset(cur,0,sizeof(cur));
+        flow+=DFS(st,inf);
+    }
+    return flow;
+}
+
+int main()
+{
+    while(~scanf("%d%d",&m,&n))
+    {
+         edges.clear();
+        for(int i=0; i<N; i++) G[i].clear();
+        s=1;t=n;
+        for(int i=0;i<m;i++)
+        {
+            int u,v,c;
+            scanf("%d%d%d",&u,&v,&c);
+            AddEdge(u,v,c);
+        }
+        printf("%d\n",Maxflow(s,t));
+    }
+    return 0;
+}
---
+++ template/图论/最小费用最大流BellmanFord.cpp
@@ -0,0 +1,103 @@
+//poj 2135 最小费用最大流
+#include<stdio.h>
+#include<string.h>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+const int N=5024;
+const int inf=0x7fffffff;
+struct Edge
+{
+    int from,to,cap,flow,cost;
+};
+vector<Edge>edges;
+vector<int>G[N];
+int n,m;
+int inq[N],p[N],d[N],a[N];
+
+void AddEdge(int from, int to,int cap, int cost)
+{
+    Edge tp;
+    tp.from=from,tp.to=to,tp.cap=cap,tp.flow=0,tp.cost=cost;
+    edges.push_back(tp);
+
+    tp.from=to,tp.to=from,tp.cap=0,tp.flow=0,tp.cost=-cost;
+    edges.push_back(tp);
+
+    int g=edges.size();
+    G[from].push_back(g-2);
+    G[to].push_back(g-1);
+}
+
+int BellmanFord(int s,int t,int &flow, int &cost)
+{
+    int i,j,u;
+    for(i=0; i<=n+1; i++) d[i]=inf;
+    memset(inq,0,sizeof(inq));
+    d[s]=0;
+    inq[s]=1;
+    p[s]=0;
+    a[s]=inf;
+    queue<int>Q;
+    Q.push(s);
+    while(!Q.empty())
+    {
+        u=Q.front();
+        Q.pop();
+        inq[u]=0;
+        for(i=0; i<G[u].size(); i++)
+        {
+            Edge &e=edges[G[u][i]];
+            if(e.cap>e.flow&&d[e.to]>d[u]+e.cost)
+            {
+                d[e.to]=d[u]+e.cost;
+                p[e.to]=G[u][i];
+                a[e.to]=min(a[u],e.cap-e.flow);
+                if(!inq[e.to])
+                {
+                    Q.push(e.to);
+                    inq[e.to]=1;
+                }
+            }
+        }
+    }
+    if(d[t]==inf) return 0;
+    flow+=a[t];
+    cost+=d[t]*a[t];
+    u=t;
+    while(u!=0)
+    {
+        edges[p[u]].flow+=a[t];
+        edges[p[u]^1].flow-=a[t];
+        u=edges[p[u]].from;
+    }
+    return 1;
+}
+
+int Mincost(int s,int t)
+{
+    int flow=0,cost=0;
+    while(BellmanFord(s,t,flow,cost));
+    return cost;
+}
+
+int main()
+{
+    int i,u,v,c;
+    while(~scanf("%d%d",&n,&m))
+    {
+        for(i=0; i<=n+1; i++) G[i].clear();
+        edges.clear();
+        for(i=0;i<m;i++)
+        {
+            scanf("%d%d%d",&u,&v,&c);
+            AddEdge(u,v,1,c);
+            AddEdge(v,u,1,c);
+        }
+        AddEdge(0,1,2,0);
+        AddEdge(n,n+1,2,0);
+        printf("%d\n",Mincost(0,n+1));
+    }
+    return 0;
+}
