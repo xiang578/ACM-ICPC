b4cc3b624e35b97f540cc118f5da413ee6095ccf
xiang578 <xiang578@foxmail.com>
2015-11-10 12:49:47 +0800

    uva

---
+++ uva/uva315.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int MAXN=1000+5;
+
+struct Edge
+{
+    int from,to;
+};
+
+vector<Edge>edges;
+vector<int>G[1000+5];
+int low[MAXN],dfn[MAXN];
+int main()
+{
+    return 0;
+}
