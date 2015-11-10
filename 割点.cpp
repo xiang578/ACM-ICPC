65c173c6f185756a7609ad04b191fa44e1c12f43
xiang578 <xiang578@foxmail.com>
2015-11-10 13:44:26 +0800

    割点-邻接表

--- 割点.cpp
+++ 割点.cpp
@@ -1,87 +1,92 @@
-//poj 1523
-#include <stdio.h>
-#include <string.h>
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <set>
-#include <map>
-#include <string>
-#include <math.h>
-#include <stdlib.h>
-#include <time.h>
+//uva 315
+//割点条件根节点有两个或两个以上节点
+//或者其他节点u有一个子女w，low[w]>=dfn[u];
+#include<bits/stdc++.h>
 using namespace std;
-const int N=1000+5;
-int g[N][N],vis[N],low[N],dfn[N],sub[N],dep,n,son;
+const int MAXN=1000+5;
+
+struct Edge
+{
+    int from,to;
+};
+
+vector<Edge>edges;
+vector<int>G[MAXN*MAXN];
+int low[MAXN],dfn[MAXN],vis[MAXN],dep,ans,sub[MAXN];
+
+void addedge(int from,int to)
+{
+    Edge tp;
+    tp.from=from;
+    tp.to=to;
+    edges.push_back(tp);
+    G[from].push_back(edges.size()-1);
+}
 
 void dfs(int u)
 {
-    for(int v=1;v<=n;v++)
+    for(int i=0; i<G[u].size(); i++)
     {
-        if(g[u][v])
+        int v=edges[G[u][i]].to;
+        if(!vis[v])
         {
-            if(!vis[v])
+            vis[v]=1;
+            dep++;
+            dfn[v]=low[v]=dep;
+            dfs(v);
+            low[u]=min(low[u],low[v]);
+            if(low[v]>=dfn[u])
             {
-                vis[v]=1;
-                dep++;
-                dfn[v]=low[v]=dep;
-                dfs(v);
-                low[u]=min(low[u],low[v]);
-                if(low[v]>=dfn[u])
-                {
-                    if(u==1) son++;
-                    else sub[u]++;
-                }
+                sub[u]++;
             }
-            else
-              low[u]=min(low[u],dfn[v]);
         }
+        else
+            low[u]=min(low[u],dfn[v]);
     }
 }
-
 int main()
 {
-    int i,j,u,v,flag,cas=0;
-    while(~scanf("%d",&u))
+    int _,i,j,u,v,c,n;
+    char st;
+    while(~scanf("%d",&n))
     {
-        n=0;
-        n=max(n,u);
-        if(u==0) break;
-        memset(g,0,sizeof(g));
-        scanf("%d",&v);
-        n=max(n,v);
-        g[u][v]=g[v][u]=1;
+        if(n==0) break;
+        ans=0;
+        edges.clear();
+        for(i=0; i<MAXN; i++)
+            G[i].clear();
         while(~scanf("%d",&u))
         {
             if(u==0) break;
-            n=max(n,u);
-            scanf("%d",&v);
-            n=max(n,v);
-            g[u][v]=g[v][u]=1;
+            while(1)
+            {
+                st=getchar();
+                if(st==10) break;
+                scanf("%d",&v);
+                addedge(u,v);
+                addedge(v,u);
+            }
         }
         memset(vis,0,sizeof(vis));
         memset(sub,0,sizeof(sub));
-        dep=1;
-        son=0;
-        dfn[1]=low[1]=vis[1]=1;
-        dfs(1);
-        if(son>1) sub[1]=son-1;
-        flag=0;
-        if(cas>0) printf("\n");
-        printf("Network #%d\n",++cas);
-        for(i=1;i<=n;i++)
+        dep=0;
+        for(i=1; i<=n; i++)
         {
-            //printf("%d\n",sub[i]);
-            if(sub[i])
+            if(vis[i]==0)
             {
-                flag=1;
-                printf("  SPF node %d leaves %d subnets\n",i,sub[i]+1);
+                vis[i]=1;
+                dep++;
+                dfn[i]=low[i]=dep;
+                dfs(i);
+                if(sub[i]>=1)sub[i]-=1;
             }
         }
-        if(!flag)
-            printf("  No SPF nodes\n");
+        for(i=1; i<=n; i++)
+        {
+            if(sub[i])
+                ans++;
+        }
+        printf("%d\n",ans);
     }
     return 0;
 }
