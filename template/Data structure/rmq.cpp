f47cf643d3b68e2823cbc690b6b8266ee70653de
xiang578 <xiang578@foxmail.com>
2015-11-12 18:34:23 +0800

    rmq

---
+++ template/Data structure/rmq.cpp
@@ -0,0 +1,26 @@
+const int MAXN=1024;
+int a[MAXN];
+int d[MAXN][20];
+
+void rmq_min(int n)
+{
+    for(int i=0;i<n;i++) d[i][0]=a[i];
+    for(int j=1;(1<<j)<n;j++)
+        for(int i=0;i+(1<<j)-1<n;i++)
+        d[i][j]=min(d[i][j-1],d[i+(1<<(j-1))][j-1]);
+}
+
+void rmq_max(int n)
+{
+    for(int i=0;i<n;i++) d[i][0]=a[i];
+    for(int j=1;(1<<j)<n;j++)
+        for(int i=0;i+(1<<j)-1<n;i++)
+        d[i][j]=max(d[i][j-1],d[i+(1<<(j-1))][j-1]);
+}
+
+void rmq(int l,int r)
+{
+    int k=0;
+    while((1<<(k+1)<=r-l+1) k++;
+    return min(d[l][k],d[r-(1<<k)+1][k]);
+}
