cf497537e4106ce3ee70f6f3fb00c3e26c2e57ff
xiangrunye-pc <xiang578@foxmail.com>
2016-01-22 14:32:56 +0800

    教育场6

--- 比赛/Educational Codeforces Round 6/B.cpp
+++ 比赛/Educational Codeforces Round 6/B.cpp
@@ -16,11 +16,10 @@ Blog          :htttp://www.xiang578.top
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
-//#include <bits/stdc++.h>
-#define rep(i,a,n) for(int i=a;i<n;i++)
-#define per(i,a,n) for(int i=n-1;i>=a;i--)
-#define pb push_back
 using namespace std;
+#define rep(i,a,n) for (int i=a;i<n;i++)
+#define per(i,a,n) for (int i=n-1;i>=a;i--)
+#define pb push_back
 typedef vector<int> VI;
 typedef long long ll;
 const ll mod=1000000007;
@@ -31,6 +30,23 @@ int main()
 {
     //freopen("in.txt","r",stdin);
     //freopen("out.txt","w",stdout);
-    
+    int a,b,p[20],t;
+    ll ans=0;
+    p[0]=6;p[1]=2;p[2]=5;p[3]=5;p[4]=4;
+    p[5]=5;p[6]=6;p[7]=3;p[8]=7;p[9]=6;
+    while(~scanf("%d%d",&a,&b))
+    {
+        ans=0;
+        rep(i,a,b+1)
+        {
+            t=i;
+            while(t)
+            {
+                ans+=p[t%10];
+                t/=10;
+            }
+        }
+        printf("%lld\n",ans);
+    }
     return 0;
 }
--- 比赛/Educational Codeforces Round 6/C.cpp
+++ 比赛/Educational Codeforces Round 6/C.cpp
@@ -26,11 +26,49 @@ typedef long long ll;
 const ll mod=1000000007;
 const int N=2048;
 
+int a[300000+10];
+int s[300000+10],e[300000+10];
 
 int main()
 {
     //freopen("in.txt","r",stdin);
     //freopen("out.txt","w",stdout);
-    
+    int i,n,st,cnt;
+    while(~scanf("%d",&n))
+    {
+        for(i=1;i<=n;i++)
+            scanf("%d",&a[i]);
+        set<int>use;
+        cnt=0;
+        st=1;
+        for(i=1;i<=n;i++)
+        {
+            if(use.find(a[i])!=use.end())
+            {
+                s[cnt]=st;
+                e[cnt]=i;
+                cnt++;
+                st=i+1;
+                use.clear();
+            }
+            else
+            {
+                use.insert(a[i]);
+            }
+        }
+        if(cnt==0)
+        {
+            printf("-1\n");
+        }
+        else
+        {
+            e[cnt-1]=n;
+            printf("%d\n",cnt);
+            rep(i,0,cnt)
+            {
+                printf("%d %d\n",s[i],e[i]);
+            }
+        }
+    }
     return 0;
 }
