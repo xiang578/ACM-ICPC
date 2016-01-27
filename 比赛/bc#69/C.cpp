e19156aa806747d7560e1fb6b18162e9089b3613
xiangrunye-pc <xiang578@foxmail.com>
2016-01-27 22:02:03 +0800

    补完bc69

--- 比赛/bc#69/C.cpp
+++ 比赛/bc#69/C.cpp
@@ -28,39 +28,105 @@ const int N=2048;
 
 int n,m,use[50][50],f;
 char mp[50][50];
-ll ans;
+ll ans,pa,pb;
+
 ll gcd(ll a,ll b)
 {
     return b==0?a:gcd(b,a%b);
 }
 
+int get(char op,ll a,ll b,int c)
+{
+    if(op=='+')
+    {
+        pa=a+b*c;
+        pb=b;
+    }
+    else if(op=='-')
+    {
+        pa=a-b*c;
+        pb=b;
+    }
+    else if(op=='*')
+    {
+        pa=a*c;
+        pb=b;
+    }
+    else if(op=='/')
+    {
+        if(c==0) return 0;
+        pa=a;
+        pb=b*c;
+    }
+    ll t=gcd(pa,pb);
+    pa=pa/t;
+    pb=pb/t;
+    return 1;
+}
+
 void dfs(int x,int y,ll a,ll b)
 {
+    //printf("%lld %lld\n",a,b);
+    if(a==b*ans||f==1) {f=1;return ;}
     use[x][y]=1;
 
+    //up
+    if(y-2>=0&&!use[x][y-2]&&mp[x][y-2]!='#')
+    {
+        //printf("%lld %lld\n",a,b);
+        if(get(mp[x][y-1],a,b,mp[x][y-2]))
+            dfs(x,y-2,pa,pb);
+    }
+
+    //down
+    if(y+2<m&&!use[x][y+2]&&mp[x][y+2]!='#')
+    {
+        //printf("%lld %lld\n",a,b)
+        if(get(mp[x][y+1],a,b,mp[x][y+2]))
+            dfs(x,y+2,pa,pb);
+    }
+
+    //left
+    if(x-2>=0&&!use[x-2][y]&&mp[x-2][y]!='#')
+    {
+        if(get(mp[x-1][y],a,b,mp[x-2][y]))
+            dfs(x-2,y,pa,pb);
+    }
+
+    //right
+    if(x+2<n&&!use[x+2][y]&&mp[x+2][y]!='#')
+    {
+        if(get(mp[x+1][y],a,b,mp[x+2][y]))
+            dfs(x+2,y,pa,pb);
+    }
+    use[x][y]=0;
 }
+
 int main()
 {
     //freopen("in.txt","r",stdin);
     //freopen("out.txt","w",stdout);
-    int _,
+    int _,i,j;
     scanf("%d",&_);
     while(_--)
     {
         scanf("%d%d%lld",&n,&m,&ans);
-        for(i=0;i<n;i++)
+        for(i=0; i<n; i++)
             scanf("%s",mp[i]);
-        for(i=0;i<n;i+=2)
-            for(j=0;j<m;j+=2)
-                mp[i][j]=mp[i][j]-'0';
+
+        for(i=0; i<n; i+=2)
+            for(j=0; j<m; j+=2)
+                if(mp[i][j]>='0'&&mp[i][j]<='9')
+                    mp[i][j]=mp[i][j]-'0';
         f=0;
-        for(i=0;i<n;i+=2)
-            for(j=0;j<m;j+=2)
-            if(0<=mp[i][j]&&mp[i][j]<=9&&!f)
-            {
-                memset(use,0,sizeof(use));
-                dfs(i,j,mp[i][j],1);
-            }
+        for(i=0; i<n; i+=2)
+            for(j=0; j<m; j+=2)
+                if(0<=mp[i][j]&&mp[i][j]<=9&&!f)
+                {
+                    memset(use,0,sizeof(use));
+                    //printf("%d\n",mp[i][j]);
+                    dfs(i,j,mp[i][j],1);
+                }
         if(f)
             printf("Possible\n");
         else
--- 比赛/bc#69/D.cpp
+++ 比赛/bc#69/D.cpp
@@ -1,64 +1,135 @@
-#include<stdio.h>
-#include<iostream>
-#include<string.h>
-#include<algorithm>
-using namespace std;
-const int N=100005;
-char t[20];
-int get(char a[20])
-{
-    int i,j;
-    for(i=6;i<=10;i++)
-        if(a[i]==a[6]) continue;
-    else break;
-    if(i>10) return 1;
-    for(i=7;i<=10;i++) if(a[i]==a[i-1]+1) continue; else break;
-    if(i>10) return 1;
-    for(i=7;i<=10;i++) if(a[i]==a[i-1]-1) continue; else break;
-    if(i>10) return 1;
-    int year=0,mo=0,day=0;
-    for(i=3;i<=6;i++) year*=10,year+=a[i]-'0';
-    for(i=7;i<=8;i++) mo*=10,mo+=a[i]-'0';
-    for(i=9;i<=10;i++) day*=10,day+=a[i]-'0';
-    if(year>=1980&&year<=2016)
-    {
-        if(mo>=1&&mo<=12)
-        {
-            if(day>=1&&day<=31)
-            {
-                if(day<=28) return 1;
-                if(mo==2&&day==29&&year%4==0) return 1;
-                if(mo==2&&day>29) return 0;
-                if(day==31)
-                {
-                    if(mo==1||mo==3||mo==5||mo==7||mo==8||mo==10||mo==12) return 1;
-                }
+/* ***********************************************
+Author        :Maltub
+Email         :xiang578@foxmail.com
+Blog          :htttp://www.xiang578.top
+************************************************ */
 
-            }
-        }
-    }
-    return 0;
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <string>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+//#include <bits/stdc++.h>
+#define rep(i,a,n) for(int i=a;i<n;i++)
+#define per(i,a,n) for(int i=n-1;i>=a;i--)
+#define pb push_back
+using namespace std;
+typedef vector<int> VI;
+typedef long long ll;
+const ll mod=1000000007;
+const int N=2048;
+
+int n,m,mp[120][120],g[120][120],ans[120][120];
+
+int get(int x,int y)
+{
+    int ret=0;
+    ret+=g[x-1][y]+g[x-2][y];
+    ret+=g[x][y-1]+g[x-1][y-1]+g[x-2][y-1];
+    ret+=g[x][y-2]+g[x-1][y-2]+g[x-2][y-2];
+    return ret;
 }
-int x,y,n;
+
 int main()
 {
-    int T;
-    scanf("%d",&T);
-    while(T--)
+    //freopen("in.txt","r",stdin);
+    //freopen("out.txt","w",stdout);
+    int _,i,j,k,t,f,sum;
+    scanf("%d",&_);
+    while(_--)
     {
-    /*while(scanf("%d",&n)!=EOF)
-    {*/
-    scanf("%d",&n);
-    scanf("%d%d",&x,&y);
-    long long ans=0;
-    while(n--)
-    {
-        scanf("%s",t);
-        int tp=get(t);
-        if(tp) ans+=x;
-        else ans+=y;
-    }
-    cout<<ans<<endl;
+        scanf("%d%d",&n,&m);
+        memset(mp,0,sizeof(mp));
+        for(i=1; i<=n; i++)
+            for(j=1; j<=m; j++)
+                scanf("%d",&mp[i][j]);
+        int cnt=1<<(n-2);
+        sum=0;
+        for(k=0; k<cnt; k++)
+        {
+            memset(g,0,sizeof(g));
+            t=k;
+            for(i=2; i<=n-1; i++)
+            {
+                g[i][1]=t%2;
+                t/=2;
+            }
+
+            for(j=1; j<m; j++)
+            {
+                for(i=1; i<n; i++)
+                {
+                    g[i+1][j+1]=mp[i][j]-get(i+1,j+1);
+                }
+            }
+
+            f=1;
+            for(i=1; i<=n; i++)
+            {
+                for(j=1; j<=m; j++)
+                {
+                    if(g[i][j]!=0&&g[i][j]!=1)
+                    {
+                        f=0;
+                        break;
+                    }
+                    if(i==1&&g[i][j]!=0)
+                    {
+                        f=0;
+                        break;
+                    }
+                    if(i==n&&g[i][j]!=0)
+                    {
+                        f=0;
+                        break;
+                    }
+                    if(mp[i][j]!=get(i+1,j+1)+g[i+1][j+1])
+                    {
+                        f=0;
+                        break;
+                    }
+                }
+                if(f!=1) break;
+            }
+
+            if(f)
+            {
+                sum++;
+                for(i=1; i<=n; i++)
+                {
+                    for(j=1; j<=m; j++)
+                    {
+                        ans[i][j]=g[i][j];
+                    }
+                }
+            }
+            if(sum>1) break;
+        }
+
+        if(sum>1)
+            puts("Multiple");
+        else if(sum==1)
+        {
+            for(i=1; i<=n; i++)
+            {
+                for(j=1; j<=m; j++)
+                {
+                    if(j!=1) printf(" ");
+                    printf("%d",ans[i][j]);
+                }
+                printf("\n");
+            }
+        }
+        else
+            puts("Impossible");
+
     }
     return 0;
 }
