bd30e1bf0a1cc9a0473e15e490a86af88f0b7087
xiangrunye-pc <xiang578@foxmail.com>
2016-02-01 21:45:31 +0800

    001

--- 比赛/cf#341/B.cpp
+++ 比赛/cf#341/B.cpp
@@ -26,11 +26,35 @@ typedef long long ll;
 const ll mod=1000000007;
 const int N=2048;
 
-
+int a[N],b[2*N];
 int main()
 {
     //freopen("in.txt","r",stdin);
     //freopen("out.txt","w",stdout);
-    
+    int i,x,y,n,ans;
+    while(~scanf("%d",&n))
+    {
+        memset(a,0,sizeof(a));
+        memset(b,0,sizeof(b));
+        for(i=0;i<n;i++)
+        {
+            scanf("%d%d",&x,&y);
+            a[x+y]++;
+            b[x-y+N]++;
+        }
+        ans=0;
+        for(i=0;i<N;i++)
+        {
+            if(a[i]>1)
+                ans+=(a[i]-1)*a[i]/2;
+        }
+        //printf("%d\n",ans);
+        for(i=0;i<2*N;i++)
+        {
+            if(b[i]>1)
+                ans+=(b[i]-1)*b[i]/2;
+        }
+        printf("%d\n",ans);
+    }
     return 0;
 }
--- 比赛/cf#341/C.cpp
+++ 比赛/cf#341/C.cpp
@@ -26,11 +26,35 @@ typedef long long ll;
 const ll mod=1000000007;
 const int N=2048;
 
+double a[100000+10];
 
 int main()
 {
     //freopen("in.txt","r",stdin);
     //freopen("out.txt","w",stdout);
-    
+    int i,n;
+    ll x,y,p,len,num;
+    double ans;
+    while(~scanf("%d%lld",&n,&p))
+    {
+        rep(i,0,n)
+        {
+            scanf("%lld%lld",&x,&y);
+            len=(y-x+1);
+            if(x%p==0)
+                x=x/p-1;
+            else
+                x=x/p;
+            y=y/p;
+            num=y-x;
+            a[i]=1.0*num/len;
+        }
+        ans=0;
+        rep(i,0,n-1)
+            ans=ans+(1-(1-a[i])*(1-a[i+1]));
+        ans=ans+(1-(1-a[n-1])*(1-a[0]));
+        ans=ans*2000;
+        printf("%.7f\n",ans);
+    }
     return 0;
 }
