38b4b8329328b92e78bc18cd4045cbafc7724d80
xiangrunye-pc <xiang578@foxmail.com>
2016-03-06 22:52:01 +0800

    不知道

--- 训练/[quanxing]2014基础母函数/hdu4651Partition.cpp
+++ 训练/[quanxing]2014基础母函数/hdu4651Partition.cpp
@@ -2,25 +2,18 @@
 using namespace std;
 const int mod=1000000007;
 const int _max = 100000+5;
-int c1[_max], c2[_max],ans[_max];
+int c1[_max], c2[_max];
 int main()
 {
     //int n,i,j,k;
-    //freopen("out.txt","w",stdout);
     int n;   //
     int i, j, k;
-    memset(ans,0,sizeof(ans));
+
     int _;
     cin>>_;
     while(_--)
     {
-        //scanf("%d",&n);
-        n=100000;
-        if(ans[n])
-        {
-            printf("%d\n",ans[n]);
-            continue;
-        }
+        scanf("%d",&n);
         for(i=0; i<=n; ++i)
         {
             c1[i] = 1;
@@ -28,11 +21,10 @@ int main()
         }
         for(i=2; i<=15; ++i)
         {
-            //printf("%d\n",i);
+
             for(j=0; j<=n; ++j)
                 for(k=0; k+j<=n; k+=i)
                 {
-                    printf("%d %d %d\n",i,j,k);
                     c2[j+k] += c1[j];
                     c2[j+k]%=mod;
                 }
@@ -42,11 +34,7 @@ int main()
                 c2[j] = 0;
             }
         }
-        for(i=0;i<=n;i++)
-        {
-           printf("c1[%d]=%d;\n",i,c1[i]);
-        }
-        //cout << c1[n] << endl;
+        cout << c1[n] << endl;
     }
     return 0;
 }
