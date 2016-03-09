fc3f259abc452434aca17239173b46455c9b0f64
xiangrunye-pc <xiang578@foxmail.com>
2016-03-09 23:07:59 +0800

    数位dp

--- 训练/[kuangbin带你飞]专题十五 数位DP/hdu4734Fx.cpp
+++ 训练/[kuangbin带你飞]专题十五 数位DP/hdu4734Fx.cpp
@@ -1,31 +1,47 @@
-#include<bits/stdc++.h>
+#include <cstdio>
+#include <string.h>
 using namespace std;
-typedef long long ll;
-int num[20],dp[20][];
-ll a[20];
-
-int dfs(int pos,ll s,int lim)
+int num[20],dp[20][200000];
+int dfs(int pos,int s,int lim)
 {
+    int ans=0,now_s,i,u;
+    if(s<0) return 0;
+    if(pos<=0) return 1;
+    if(!lim&&dp[pos][s]!=-1)   return dp[pos][s];
 
+    u=lim?num[pos]:9;
+    for(i=0;i<=u;i++)
+    {
+        now_s=s-i*(1<<(pos-1));
+        ans+=dfs(pos-1,now_s,lim&&i==u);
+    }
+    if(!lim) dp[pos][s]=ans;
+    return ans;
 }
 
 int main()
 {
-    int _,n,i;
-    a[0]=1;
-    for(i=1;i<=10;i++)
-        a[i]=2*a[i-1];
+    int _,t,a,b,i,j,tmp;
     scanf("%d",&_);
-    while(_--)
+    memset(dp,0xff,sizeof(dp));
+    for(t=1;t<=_;t++)
     {
-        scanf("%d",&n);
+        scanf("%d%d",&a,&b);
         i=0;
-        while(n)
+        while(b)
         {
-            num[++i]=n%10;
-            n/10;
+            num[++i]=b%10;
+            b/=10;
         }
-        printf("%d\n",dfs(i,))
+        tmp=0;
+        j=1;
+        while(a)
+        {
+            tmp+=j*(a%10);
+            a/=10;
+            j<<=1;
+        }
+        printf("Case #%d: %d\n",t,dfs(i,tmp,1));
     }
     return 0;
 }
