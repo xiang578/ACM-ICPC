f01b8a85e06a9fea0a1e9270be4428bc956c45e2
xiang578 <xiang578@foxmail.com>
2016-03-14 10:24:31 +0800

    修改

--- do/uestc482-搜索-优先队列优化.cpp
+++ do/uestc482-搜索-优先队列优化.cpp
@@ -1,8 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N=100000+10;
+const int N=100000+1;
 typedef long long ll;
-
 struct edge
 {
     ll u,v,w;
@@ -12,37 +11,37 @@ struct node
     ll m,t;
     friend bool operator <(node n1,node n2)
     {
-        return n1.m<n2.m;
+        return n2.t<n1.t;
     }
-}p,o;
+};
 int n;
-
 int cmp(edge a,edge b)
 {
-    return a.u<b.u;
+    return a.v<b.v;
 }
 
 ll bfs(ll m)
 {
-    int i;
+    int i,st=0;
     priority_queue<node>pq;
+    node o,p;
     p.m=1;
     p.t=0;
     pq.push(p);
     while(!pq.empty())
     {
         p=pq.top();
-        //printf("%d %d\n",p.m,p.t);
         pq.pop();
         if(p.m>=m) return p.t;
-        for(i=0;i<n;i++)
+        for(i=st;i<n;i++)
         {
-            if(g[i].u>p.m) break;
-            o.m=g[i].v;
+            if(g[i].v>p.m) break;
+            if(g[i].u<=p.m) continue;
+            o.m=g[i].u;
             o.t=p.t+g[i].w;
-            printf("%d %d\n",o.m,o.t);
             pq.push(o);
         }
+        st=i;
     }
     return -1;
 }
@@ -56,9 +55,7 @@ int main()
     {
         scanf("%d%lld",&n,&m);
         for(i=0;i<n;i++)
-        {
             scanf("%lld%lld%lld",&g[i].u,&g[i].v,&g[i].w);
-        }
         sort(g,g+n,cmp);
         printf("Case #%d: %lld\n",++cas,bfs(m));
     }
