7f1023133239ba0484c081b72e1ef042895db946
xiang578 <xiang578@foxmail.com>
2016-03-25 12:00:24 +0800

    update

--- do/hdu1505.cpp
+++ do/hdu1505.cpp
@@ -1,31 +1,60 @@
-#include <stdio.h>
+#include<bits/stdc++.h>
 using namespace std;
-const int N=100000+10;
-__int64 a[N],l[N],r[N],tmp;
+const int N=1024;
+int g[N][N],r[N][N],h[N][N],l[N][N];
 
 int main()
 {
-    int n,i;
-    while(~scanf("%d",&n)&&n)
+    int _,n,m,i,j,k,ans;
+    char s[4000];
+    scanf("%d",&_);
+    while(_--)
     {
-        for(i=1; i<=n; i++)
+        scanf("%d%d",&n,&m);
+        //getchar();
+        for(i=1;i<=n;i++)
         {
-            scanf("%I64d",&a[i]);
-            l[i]=r[i]=i;
+            for(j=1;j<=m;j++)
+            {
+                scanf("%s",s);
+                if(s[0]=='R') g[i][j]=0;
+                else g[i][j]=1;
+                r[i][j]=l[i][j]=j;
+            }
         }
-        a[0]=a[n+1]=-1;
-        for(i=1; i<=n; i++)
+        memset(h,0,sizeof(h));
+        for(i=1;i<=n;i++)
         {
-            while(a[l[i]-1]>=a[i]) l[i]=l[l[i]-1];
+            for(j=1;j<=m;j++)
+            {
+                if(g[i][j]==0) h[i][j]=0;
+                else h[i][j]=h[i-1][j]+1;
+            }
         }
-        __int64 ans=0;
-        for(i=n; i>=1; i--)
+        ans=0;
+        //printf("11111\n");
+        for(i=1;i<=n;i++)
         {
-            while(a[r[i]+1]>=a[i]) r[i]=r[r[i]+1];
-            tmp=(r[i]-l[i]+1)*a[i];
-            if(tmp>=ans) ans=tmp;
+            //printf("%d",i);
+            h[i][0]=h[i][m+1]=-1;
+            for(j=m;j>=1;j--)
+            {
+                while(h[i][r[i][j]+1]>=h[i][r[i][j]])
+                    r[i][j]=r[i][r[i][j]+1];
+            }
+
+            for(j=1;j<=m;j++)
+            {
+                while(h[i][l[i][j]-1]>=h[i][l[i][j]])
+                    l[i][j]=l[i][l[i][j]-1];
+            }
+            for(j=1;j<=m;j++)
+            {
+                //printf("%d %d %d %d\n",(l[i][j]-r[i][j]+1)*h[i][j],r[i][j],l[i][j],h[i][j]);
+                ans=max(ans,(r[i][j]-l[i][j]+1)*h[i][j]);
+            }
         }
-        printf("%I64d\n",ans);
+        printf("%d\n",3*ans);
     }
     return 0;
 }
