7d577301ff7f67a0ef19199b722690e5ad131728
xiangrunye-pc <xiang578@foxmail.com>
2016-03-28 18:00:07 +0800

    undo

--- Training/Educational Codeforces Round 10/A.cpp
+++ Training/Educational Codeforces Round 10/A.cpp
@@ -3,34 +3,30 @@ Author        :Maltub
 Email         :xiang578@foxmail.com
 Blog          :htttp://www.xiang578.top
 ************************************************ */
-
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
-#include <set>
-#include <map>
-#include <string>
-#include <math.h>
-#include <stdlib.h>
-#include <time.h>
-//#include <bits/stdc++.h>
-#define rep(i,a,n) for(int i=a;i<n;i++)
-#define per(i,a,n) for(int i=n-1;i>=a;i--)
-#define pb push_back
+#include <bits/stdc++.h>
 using namespace std;
-typedef vector<int> VI;
-typedef long long ll;
-const ll mod=1000000007;
-const int N=2048;
-
 
 int main()
 {
     //freopen("in.txt","r",stdin);
     //freopen("out.txt","w",stdout);
-    
+    long long  h1,h2,a,b,now,ans;
+    while(~scanf("%I64d%I64d%I64d%I64d",&h1,&h2,&a,&b))
+    {
+        now=h1;
+        ans=0;
+        now+=8*a;
+        if(now>=h2) {printf("%I64d\n",ans);continue;}
+        now-=12*b;
+        if(a<=b&&12*a+now<h2) {printf("-1\n");continue;}
+        while(1)
+        {
+            ans++;
+            now+=12*a;
+            if(now>=h2) break;
+            now-=12*b;
+         }
+        printf("%I64d\n",ans);
+    }
     return 0;
 }
--- Training/Educational Codeforces Round 10/D.cpp
+++ Training/Educational Codeforces Round 10/D.cpp
@@ -24,13 +24,65 @@ using namespace std;
 typedef vector<int> VI;
 typedef long long ll;
 const ll mod=1000000007;
-const int N=2048;
+const int N=2*(1e5)+10;
+const int up=2*N;
+int c[up+10];
+int l[N],r[N];
+map<int,int>mp;
+int lowbit(int x)
+{
+    return x&-x;
+}
 
+int sum(int x)
+{
+    int ret=0;
+    while(x>0)
+    {ret+=c[x];
+    x-=lowbit(x);
+    }
+    return ret;
+}
+
+void add(int x,int d)
+{
+    while(x<=up)
+    {
+        c[x]+=d;
+        x+=lowbit(x);
+    }
+}
 
 int main()
 {
     //freopen("in.txt","r",stdin);
     //freopen("out.txt","w",stdout);
-    
+    int i,n,cnt,tl,tr;
+    scanf("%d",&n);
+    for(i=0;i<=up;i++) c[up]=0;
+    cnt=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d%d",&l[i],&r[i]);
+        x[cnt]=l[i];
+        cnt++;
+        x[cnt]=r[i];
+        cnt++;
+    }
+    mp.clear();
+    sort(x,x+cnt);
+    for(i=0;i<cnt;i++)
+    {
+        mp[x[i]]=i+1;
+    }
+    for(i=0;i<cnt;i++)
+    {
+        tr=mp[r[i]];
+        add(tr,1);
+    }
+    for(i=0;i<cnt;i++)
+    {
+        printf("%d\n",sum(mp[r[i])-sum(mp[l[i]]));
+    }
     return 0;
 }
