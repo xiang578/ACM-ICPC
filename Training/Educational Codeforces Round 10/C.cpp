76ab84b2913d613795b24827c5cae9dd6aa1a1df
Rick Xiang <xiang578@foxmail.com>
2016-03-30 08:59:59 +0800

    Update C.cpp

--- Training/Educational Codeforces Round 10/C.cpp
+++ Training/Educational Codeforces Round 10/C.cpp
@@ -1,88 +1,38 @@
-/* ***********************************************
-Author        :Maltub
-Email         :xiang578@foxmail.com
-Blog          :htttp://www.xiang578.top
-************************************************ */
-
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
-#include <set>
-#include <map>
-#include <string>
-#include <math.h>
-#include <stdlib.h>
-#include <time.h>
-//#include <bits/stdc++.h>
-#define rep(i,a,n) for(int i=a;i<n;i++)
-#define per(i,a,n) for(int i=n-1;i>=a;i--)
-#define pb push_back
+#include<bits/stdc++.h>
 using namespace std;
-typedef vector<int> VI;
 typedef long long ll;
-const ll mod=1000000007;
-const int N=2*(1e5)+10;
-const int up=2*N;
-int c[up+10];
-int l[N],r[N];
-map<int,int>mp;
-int lowbit(int x)
-{
-    return x&-x;
-}
-
-int sum(int x)
-{
-    int ret=0;
-    while(x>0)
-    {ret+=c[x];
-    x-=lowbit(x);
-    }
-    return ret;
-}
-
-void add(int x,int d)
-{
-    while(x<=up)
-    {
-        c[x]+=d;
-        x+=lowbit(x);
-    }
-}
+const int N=3*1e5+10;
+int n,m,p[N],pos[N],up[N];
 
 int main()
 {
-    //freopen("in.txt","r",stdin);
-    //freopen("out.txt","w",stdout);
-    int i,n,cnt,tl,tr;
-    scanf("%d",&n);
-    for(i=0;i<=up;i++) c[up]=0;
-    cnt=0;
-    for(i=0;i<n;i++)
+    int i,a,b,x,y;
+    ll ans;
+    while(~scanf("%d%d",&n,&m))
     {
-        scanf("%d%d",&l[i],&r[i]);
-        x[cnt]=l[i];
-        cnt++;
-        x[cnt]=r[i];
-        cnt++;
-    }
-    mp.clear();
-    sort(x,x+cnt);
-    for(i=0;i<cnt;i++)
-    {
-        mp[x[i]]=i+1;
-    }
-    for(i=0;i<cnt;i++)
-    {
-        tr=mp[r[i]];
-        add(tr,1);
-    }
-    for(i=0;i<cnt;i++)
-    {
-        printf("%d\n",sum(mp[r[i])-sum(mp[l[i]]));
+        for(i=1;i<=n;i++)
+            up[i]=n;
+        for(i=1;i<=n;i++)
+        {
+            scanf("%d",&p[i]);
+            pos[p[i]]=i;
+        }
+        for(i=0;i<m;i++)
+        {
+            scanf("%d%d",&a,&b);
+            x=pos[a];
+            y=pos[b];
+            if(x>y) swap(x,y);
+            up[x]=min(up[x],y-1);
+        }
+        for(i=n-1;i>=1;i--)
+        {
+            up[i]=min(up[i],up[i+1]);
+        }
+        ans=0;
+        for(i=1;i<=n;i++)
+            ans+=(up[i]-i+1);
+        cout<<ans<<endl;
     }
     return 0;
 }
