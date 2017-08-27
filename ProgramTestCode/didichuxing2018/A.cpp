4eceb7f944931eec22a4088f1e1eb5ee5fa4cd01
xiang578 <xiang578@foxmail.com>
2017-08-27 10:14:26 +0800

    didichuxing2018

--- ProgramTestCode/didichuxing2018/A.cpp
+++ ProgramTestCode/didichuxing2018/A.cpp
@@ -24,13 +24,29 @@ using namespace std;
 typedef vector<int> VI;
 typedef long long ll;
 const ll mod=1000000007;
-const int N=2048;
-const int M=1e6+10;
+const int N=20480;
+const int M=1e5+10;
+int a[N];
+int dp[N];
 
 int main()
 {
     //freopen("in.txt","r",stdin);
     //freopen("out.txt","w",stdout);
-
+    int n = 0;
+    char ch;
+	while(scanf("%d%c",a+n,&ch)){
+		n++;
+		if(ch=='\n') break;
+	}
+	dp[0]=a[0];
+	int ans = a[0];
+	for(int i=1;i<n;i++)
+	{
+		dp[i] = max(a[i],dp[i-1]+a[i]);
+		ans = max(ans,a[i]);
+		ans = max(ans,dp[i]);
+	}
+	printf("%d\n",ans);
     return 0;
 }
--- ProgramTestCode/didichuxing2018/B.cpp
+++ ProgramTestCode/didichuxing2018/B.cpp
@@ -24,13 +24,53 @@ using namespace std;
 typedef vector<int> VI;
 typedef long long ll;
 const ll mod=1000000007;
-const int N=2048;
-const int M=1e5+10;
+const int N=20480;
+const int M=1e6+10;
+int a[N],n,k,ans;
 
-int main()
-{
+
+
+int Partition(int l,int r) {
+	int pivot = a[l];
+	while(l<r)
+	{
+		while(l<r&&a[r]>pivot) r--;
+		a[l] = a[r];
+		while(l<r&&a[l]<pivot) l++;
+		a[r] = a[l];
+	}
+	a[l] = pivot;
+	return l;
+}
+
+void get(int l,int r) {
+	if(l<r) {
+		int postion = Partition(l,r);
+		if(postion==k) {ans=a[k]; return ;}
+		if(postion<k) 
+			get(postion+1,r);
+		else
+			get(l,postion-1);
+	}
+	if(l==r)
+	{
+		ans = a[k];return ;
+	}
+}
+
+int main() {
     //freopen("in.txt","r",stdin);
     //freopen("out.txt","w",stdout);
-
+	n = 0;
+    char ch;
+	while(scanf("%d%c",a+n,&ch)) {
+		n++;
+		if(ch=='\n') 
+			break;
+	}
+	scanf("%d",&k);
+	k=n-k;
+	get(0,n-1);
+	printf("%d\n",ans);
     return 0;
 }
