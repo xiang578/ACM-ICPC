6440e39137e0faa30da8d2d01d5849de65d67ebc
xiang578 <xiang578@foxmail.com>
2021-06-29 19:52:55 +0800

    fix

---
+++ leetcode/1563.石子游戏-v.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1563 lang=cpp
+ *
+ * [1563] 石子游戏 V
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int stoneGameV(vector<int>& stoneValue) {
+
+    }
+};
+// @lc code=end
+
